8d7f95b29df9860703c0ba55cbc6cd085eb6d58a
MonicaD <Monicadiaz468@Outlook.es>
2018-09-14 16:50:19 -0500

    hola

--- Project1/index.php
+++ Project1/index.php
@@ -14,15 +14,55 @@ and open the template in the editor.
         <h1>
             <?php
              // Esto es Un comentario / variables
-             $PrimeraVariable="Soy Una Variable";
-             echo "<br>";
-             echo $PrimeraVariable."<br>";
-             $SegundaVariable=1234;
-             echo $SegundaVariable;
+            $PrimeraVariable=2;
+            $SegundaVariable=5;
+            $lenguaje="Javascipt";
+          
+          //otra manera
+          //if ($PrimeraVariable ==2 && $SegundaVariable==5{}
+           if ($PrimeraVariable <1){
+               echo 'Soy menor';
+           }
+           else{
+               echo 'Soy Mayor';
+           }
+           switch ($lenguaje) {
+               case "PHP":
+                   echo 'PHP';
+                   break;
+               case"Javascript":
+                   echo 'Javascript';
+                   break;
+               case"html":
+                   echo 'html';
+                   break;
+           }
+           echo '<br>';
+           $Arreglo=array ("Hola",1,"hello");
+           //print_r($Arreglo);
+          // echo'Arreglo'[2]."<br>".$Arreglo[0];
+            //Arreglos 
+           //echo $segundavariable
              ?>  
+            <h1>
+             <pre>
+                <h3>
+                    <?PHP
+                   var_dump($Arreglo);// print_r($Arreglo)
+                   $Arreglo="Pyton";
+                    var_dump($Arreglo);
+                    
+                    $Usuarios=array ('Nombre'=> "Juan","Apellido"=> "Zabala","Cedula"=> 1234);
+                    echo "<h1>".$Usuarios['Nombre']."</h1>";
+                    
+                    ?>
+                </h3>
+             </pre>
+            </h1>
         </h1>
         <p>
             <h2>
+                ////54848484845554545
                 <?PHP
                     echo 'Esta es Otra Sección';
                  ?>
