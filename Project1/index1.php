fd440a3d722807f448a9270ce9a35b5a2045b509
Monicadiaz <Monicadiaz468@Outlook.es>
2018-09-19 14:01:07 -0500

    nuevos 1

---
+++ Project1/index1.php
@@ -0,0 +1,31 @@
+<pre>
+	<?php
+		$lenguajes=['html',
+			'css','javascript'];
+		print_r($lenguajes);
+		echo "<br>";
+		var_dump($lenguajes)
+	?>
+
+      <?php
+                //Funcion Sin parámetros
+                function saludar(){
+                    echo'<h2>'.'Hola A Todos'.'</h2';
+                }
+                saludar();
+                //Función con parámetros
+
+            function Usuario ($Nombres,$Tel){ 
+                print_r($Nombres);
+               /* foreach ($Nombres as $Nombres){
+                    echo "Nombres:".$Nombres. '<br>';        
+                }*/
+                echo 'Telefono:'.$Tel.'<br>';
+            }
+
+            echo "<br>";
+            $Usuarios=["Juan","Felipe","Alejandro"];
+            Usuario($Usuarios,1234);
+    ?>
+
+</pre>
