b7be7207a549f808ab652b76ed5d108cb645f0c4
Monicadiaz <Monicadiaz468@Outlook.es>
2018-09-19 16:00:49 -0500

    Get y varipag

---
+++ Practica3/Registro.php
@@ -0,0 +1,12 @@
+<?php
+    //Obtener datos por get-los trae un array asociativo 
+    $Usuario=$_GET;
+    var_dump($Usuario);
+    //Solo un campo  es: echo (variable['Nombre']); posición 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
---
+++ Practica3/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="Registro.php" method="get">
+            <label>Nombre</label>
+            <input type="text" name="Nombre" placeholder="nombre">
+            <br> <br>
+            <label>Apellido</label>
+            <input type="text" name="Apellido" placeholder="Apellido">
+            <br> <br> 
+            <label>Telefono</label>
+            <input type="text" name="Telefono" placeholder="Telefono">
+             <br> <br> 
+            <input type="submit"   value="Enviar">  
+        </form>
+        <?php
+        
+
+        ?>
+    </body>
+</html>
