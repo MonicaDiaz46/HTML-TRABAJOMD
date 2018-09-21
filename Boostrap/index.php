7cbeda765d6125a96976ff77d0647d30b1a92e88
Monicadiaz <Monicadiaz468@Outlook.es>
2018-09-21 16:56:28 -0500

    otros cambios

--- Boostrap/index.php
+++ Boostrap/index.php
@@ -25,6 +25,85 @@ and open the template in the editor.
               </a>
             </nav>
         </header>
+        <section>
+            <div class="container ">
+                <div class="registro">
+                    <img class="img-responsive" src="IMG/Usua.jpg" alt="">
+                </div>
+                    <form>
+                       <div class="form-row">
+                         <div class="form-group col-md-6">
+                           <label for="Nombre">Nombre </label>
+                           <input type="text" class="form-control" id="Nombre" placeholder="Ingrese Nombre" name="Nombre">
+                         </div>
+                         <div class="form-group col-md-6">
+                            <label for="Apellido">Apellido</label>
+                            <input type="text" class="form-control" id="Apellido" placeholder="Ingrese Apellido" name="Apellido">
+                         </div>
+                       </div>
+                       <div class="form-row">
+                         <div class="form-group col-md-6">
+                           <label for="Pais">Pais</label>
+                           <select class="form-control" id="Pais" name="Pais">
+                               <option value="Colombia" >Colombia</option>
+                               <option value="Venezuela">Venezuela</option>
+                               <option value="Ecuador">Ecuador</option>
+                               <option value="EE.UU">EE.UU</option>
+                               <option value="Suiza">Suiza</option>
+                          </select>
+                         </div>
+                         <div class="form-group col-md-4">
+                          <label for="Departamento">Departamento</label>
+                          <select class="form-control" id="Departamento" name="Departamento">
+                              <option value="Ant">Antioqia</option>
+                              <option value="Atlantico">Atlantico</option>
+                              <option value="Cundinamarca">Cundinamarca</option>
+                              <option value="Arauca">Arauca</option>
+                              <option value="Magdalena">Magdalena</option>
+                         </select>
+                         </div>
+                         <div class="form-group col-md-2">
+                           <label for="Ciudad">Ciudad</label>
+                           <select class="form-control" id="Ciudad" name="">
+                               <option value=" Medellin">Medellin</option>
+                               <option value=" Bucaramanga">Bucaramanga</option>
+                               <option value="Barranquilla">Barranquilla</option>
+                               <option value="Manizales">Manizales</option>
+                               <option value="Bogota">Bogota</option>
+                           </select>
+                         </div>
+                       </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                   <label for="Telefono">Telefono </label>
+                                   <input type="text" class="form-control" id="Telefono" placeholder="Ingrese Telefono" name="Telefono">
+                               </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="Email">Email </label>
+                                    <input type="email" class="form-control" id="Email" placeholder="Ingrese Email" name="Email">
+                                </div>
+                            </div>
+                        </div>
+                       <div class="form-group">
+                         <div class="form-check">
+                           <input class="form-check-input" type="checkbox" id="Terminoscondiciones">
+                           <label class="form-check-label" for="gridCheck">
+                             Terminos y Condiciones
+                           </label>
+                         </div>
+                       </div>
+                       <button type="submit" class="btn btn-primary btn-enviar">Completar Registro</button>
+                     </form>
+            </div>
+        </section>
+        <footer>
+            <div  class="container-fluid  navbar style-footer">
+                <h2> &COPY Derechos Reservados</h2>
+            </div>
+        </footer>
         <?php
         // put your code here
         ?>
