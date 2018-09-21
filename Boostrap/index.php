cf9869ec50a1079d59e66d9e7671ced4f27b6b0f
Monicadiaz <Monicadiaz468@Outlook.es>
2018-09-21 14:11:18 -0500

    Boostrap, js, pooper

---
+++ Boostrap/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title></title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/EstilosIndex.css"/>
+        
+        
+    </head>
+    <body>
+        <header>
+            <!-- Image and text -->
+            <nav  class="navbar navbar-light ">
+              <a class="navbar-brand" href="#">
+                  <img src="IMG/Logo.jpg" width="40" height="40" class="d-inline-block align-top" alt="">
+                Bootstrap
+              </a>
+            </nav>
+        </header>
+        <?php
+        // put your code here
+        ?>
+        <script src="js/jquery-3.3.1.min.js"> </script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
